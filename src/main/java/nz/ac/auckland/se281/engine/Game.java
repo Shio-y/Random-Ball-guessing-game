ab2684587dc12f76452a74ecaace46494917cc9f
Jie Xin Huang <jhua455@aucklanduni.ac.nz>
2025-05-20 03:12:40 +1200

    play() now prints confirmation message when valid inputsentered

--- src/main/java/nz/ac/auckland/se281/engine/Game.java
+++ src/main/java/nz/ac/auckland/se281/engine/Game.java
@@ -3,65 +3,55 @@ package nz.ac.auckland.se281.engine;
 import nz.ac.auckland.se281.Main.Difficulty;
 import nz.ac.auckland.se281.cli.MessageCli;
 import nz.ac.auckland.se281.model.Colour;
-import nz.ac.auckland.se281.engine.Valid;
 
 public class Game {
   public static String AI_NAME = "HAL-9000";
   public int maxRounds = 0;
   public int currentRound = 0;
+  public String playerName = null;
 
-  public Game() {
-
-
-  }
+  public Game() {}
 
   public void newGame(Difficulty difficulty, int numRounds, String[] options) {
-    maxRounds = numRounds; //set the max number of rounds
-   
-    MessageCli.WELCOME_PLAYER.printMessage(options[0]);
-    
+    maxRounds = numRounds; // set the max number of rounds
+
+    playerName = options[0];
+    MessageCli.WELCOME_PLAYER.printMessage(playerName);
   }
 
   public void play() {
-    Colour chosenColour;
-    Colour guessColour;
+    Colour chosenColour = null;
+    Colour guessColour = null;
     String[] input;
     Boolean validInputs = false;
-    
-    
-    currentRound +=1; //increments the round counter
-    MessageCli.START_ROUND.printMessage(currentRound,maxRounds);
 
-    
-    while (validInputs == false){
+    currentRound += 1; // increments the round counter
+    MessageCli.START_ROUND.printMessage(currentRound, maxRounds);
+
+    while (validInputs == false) {
       MessageCli.ASK_HUMAN_INPUT.printMessage();
-      //splits the input and then attempts to turn them into colour enum
-      input = (Interact.readStringInput().split(" ")); 
 
-      //checks if there are enough inputs
-      if (input.length!=2){
+      input = (Interact.readStringInput().split(" "));
+
+      // checks if there are enough inputs
+      if (input.length != 2) {
         MessageCli.INVALID_HUMAN_INPUT.printMessage();
         continue;
       }
 
-      //attempts to turn inputs into colour enum
+      // attempts to turn inputs into colour enum
       chosenColour = Colour.fromInput(input[0]);
       guessColour = Colour.fromInput(input[1]);
 
-      //checks if both inputs are valid and outputs error message if not
-      if (Valid.checkColour(chosenColour)&& Valid.checkColour(guessColour)){
+      // checks if both inputs are valid and outputs error message if not
+      if (Valid.checkColour(chosenColour) && Valid.checkColour(guessColour)) {
         break;
-      }else{
+      } else {
         MessageCli.INVALID_HUMAN_INPUT.printMessage();
       }
-
     }
-    
-      
-    
-
-
-    
+    MessageCli.PRINT_INFO_MOVE.printMessage(
+        playerName, chosenColour.toString(), guessColour.toString());
   }
 
   public void showStats() {}
--- src/main/java/nz/ac/auckland/se281/engine/Valid.java
+++ src/main/java/nz/ac/auckland/se281/engine/Valid.java
@@ -1,15 +1,13 @@
 package nz.ac.auckland.se281.engine;
+
 import nz.ac.auckland.se281.model.Colour;
 
 public class Valid {
-    
-    public static boolean checkColour(Colour colour){
-        if (colour != null){
-            return true;
-        }
-        return false;
-            
-        
+
+  public static boolean checkColour(Colour colour) {
+    if (colour != null) {
+      return true;
     }
-    
+    return false;
+  }
 }
