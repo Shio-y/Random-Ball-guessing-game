408b9517ffe87712cca32a1e0a4535d24fbb1ba4
Jie Xin Huang <jhua455@aucklanduni.ac.nz>
2025-05-20 02:26:11 +1200

    play() asks for inputs and outputs error message if invalid colour

--- src/main/java/nz/ac/auckland/se281/engine/Game.java
+++ src/main/java/nz/ac/auckland/se281/engine/Game.java
@@ -2,6 +2,7 @@ package nz.ac.auckland.se281.engine;
 
 import nz.ac.auckland.se281.Main.Difficulty;
 import nz.ac.auckland.se281.cli.MessageCli;
+import nz.ac.auckland.se281.model.Colour;
 
 public class Game {
   public static String AI_NAME = "HAL-9000";
@@ -21,8 +22,20 @@ public class Game {
   }
 
   public void play() {
+    Colour currentColour;
+    
     currentRound +=1; //increments the round counter
     MessageCli.START_ROUND.printMessage(currentRound,maxRounds);
+
+    MessageCli.ASK_HUMAN_INPUT.printMessage();
+    currentColour = Colour.fromInput(Interact.readStringInput());
+
+    if (currentColour == null){
+      MessageCli.INVALID_HUMAN_INPUT.printMessage();
+    }
+
+
+    
   }
 
   public void showStats() {}
