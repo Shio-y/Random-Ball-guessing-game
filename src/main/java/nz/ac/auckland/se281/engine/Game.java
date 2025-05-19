dc4158be540d9cf2b8e2b140ba429d487ded4723
Jie Xin Huang <jhua455@aucklanduni.ac.nz>
2025-05-20 00:59:02 +1200

    added round tracking using global variables maxRound

--- src/main/java/nz/ac/auckland/se281/engine/Game.java
+++ src/main/java/nz/ac/auckland/se281/engine/Game.java
@@ -5,6 +5,8 @@ import nz.ac.auckland.se281.cli.MessageCli;
 
 public class Game {
   public static String AI_NAME = "HAL-9000";
+  public int maxRounds = 0;
+  public int currentRound = 0;
 
   public Game() {
 
@@ -12,11 +14,16 @@ public class Game {
   }
 
   public void newGame(Difficulty difficulty, int numRounds, String[] options) {
+    maxRounds = numRounds; //set the max number of rounds
    
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    
   }
 
-  public void play() {}
+  public void play() {
+    currentRound +=1; //increments the round counter
+    MessageCli.START_ROUND.printMessage(currentRound,maxRounds);
+  }
 
   public void showStats() {}
 }
--- src/main/java/nz/ac/auckland/se281/engine/Interact.java
+++ src/main/java/nz/ac/auckland/se281/engine/Interact.java
@@ -5,7 +5,7 @@ import nz.ac.auckland.se281.cli.Utils;
 public class Interact {
 
     public static String readStringInput(){
-        
+        //this reads the next line inputted by the user.
         String placeholder = Utils.scanner.nextLine().toString();
         return placeholder;
     }
